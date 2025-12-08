0f6499c75273eb080b4afcdab373a9c228aad41b
tolegen <skverno07@gmail.com>
2025-12-08 11:37:30 +0500

    feat: добавлен endpoint api/users/me/ и подключен api-auth

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -10,3 +13,12 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=['get'], url_path='me', permission_classes=[IsAuthenticated],)
+    def me(self, request):
+        """
+        Возвращает данные текущего авторизованного пользователя
+        Используем тот же сериализатор, что и для User
+        """
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)

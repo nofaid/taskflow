c4ea533611975e9c35a421d205b6d92c8f295d30
tolegen <skverno07@gmail.com>
2025-12-08 14:43:26 +0500

    feat: добавлен бизнес-домен projects и модель Project

---
+++ apps/projects/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Project(models.Model):
+    """
+    Бизнес домен: проект
+    минимальное количество полей
+    """
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='projects',
+        verbose_name='Владелец',
+    )
+
+    name = models.CharField("Название", max_length=255)
+    description = models.TextField("Описание", blank=True)
+    created_at = models.DateTimeField("Создано", auto_now_add=True)
+    updated_at = models.DateTimeField("Обновлено", auto_now=True)
+
+    class Meta:
+        verbose_name = "Владелец",
+        verbose_name_plural = "Проекты",
+        ordering=("-created_at",)
+
+    def __str__(self):
+        return self.name
--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'apps.users',
+    'apps.projects',
 ]
 
 AUTH_USER_MODEL = "users.User"

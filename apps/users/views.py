651af6ebf2550ce5df2bfaf2243c54beb7cad813
tolegen <skverno07@gmail.com>
2025-12-08 10:52:34 +0500

    feat: добавлен первый DRF API для пользователей

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Read-only API для пользователя
+    """
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer

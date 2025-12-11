b89690a5d4b559f2a76019d5059600bde92915e0
tolegen <skverno07@gmail.com>
2025-12-11 15:52:13 +0500

    feat: бизнес-правила обновления задач (нельзя менять porject)

--- apps/tasks/services.py
+++ apps/tasks/services.py
@@ -17,3 +17,16 @@ def create_task_for_user(*, user, data: dict[str, Any]) -> Task:
     task = Task.objects.create(**data)
     return task
 
+@transaction.atomic
+def update_task_for_user(*, user, task: Task, data: dict[str, Any]) -> Task:
+    if task.project.owner != user:
+        raise PermissionDenied('Нельзя редактировать задачи в чужом проекте!')
+
+    if 'project' in data and data['project'] != task.project:
+        raise PermissionDenied('Нельзя менять проект у существующей задачи!')
+
+    for field, value in data.items():
+        setattr(task, field, value)
+
+    task.save()
+    return task
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Task
 from .serializers import TaskSerializer
-from .services import create_task_for_user
+from .services import create_task_for_user, update_task_for_user
 
 # Create your views here.
 
@@ -17,3 +17,6 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         create_task_for_user(user=self.request.user, data=serializer.validated_data)
+
+    def perform_update(self, serializer):
+        update_task_for_user(user=self.request.user, task=self.get_object(), data=serializer.validated_data)
